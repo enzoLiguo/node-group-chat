e521b55f2b80ff493b918c176faf947cdfc62516
enzoLiguo <43888872+enzoLiguo@users.noreply.github.com>
2019-10-11 11:07:44 +0200

    Update

--- index.js
+++ index.js
@@ -68,9 +68,10 @@ clientDB.connect();
 
 //Connecting to PostgreSQL Database
 const {Client} = require('pg');
-var connectionString = 'postgres://'+ process.env.DB_USER +':'+process.env.DB_PASSWORD+'@'+process.env.DB_HOST+':'+process.env.DB_PORT+'/'+process.env.DB_DATABASE+'';
+//var connectionString = 'postgres://'+ process.env.DB_USER +':'+process.env.DB_PASSWORD+'@'+process.env.DB_HOST+':'+process.env.DB_PORT+'/'+process.env.DB_DATABASE+'';
+var connectionString = process.env.DATABASE_URL;
 var clientDB = new Client({
-  connectionString: process.env.DATABASE_URL
+  connectionString: connectionString
 });
 clientDB.connect();
 
